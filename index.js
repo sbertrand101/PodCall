867ad63b56c61b0daff11fd2c10b3b1789e4c7d1
Joshua Horwitz <jhorwitz@bandwidth.com>
2015-07-13 15:34:09 -0400

    Added to Router.js so a podcast plays when a phone number is called

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 
 var express = require("express");
 var Router = require("./lib/Router");
+var bodyParser = require("body-parser");
 var app = express();
 
 var router = new Router();
+app.use(bodyParser.json());
 app.use("/", router.router);
-
-router.initialize().then(function () {});
 
 //the app is locally hosted at port 8081, or set to an environmental variable by the host
 var server = app.listen(process.env.PORT || 8081, function () {
